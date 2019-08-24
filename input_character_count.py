11609383a9d183b3114618a297531bc6bddb0b8a
irrlicht-anders <irrlicht.anders@posteo.me>
2019-08-24 15:11:13 +0200

    New File input_character_count.py

---
+++ input_character_count.py
@@ -0,0 +1,5 @@
+## This Program will give you an output of the last three characters of the input you' ve done ##
+
+usr = input("Input your text, please.")
+x = usr[-3:]
+print ("The last three characters of your input are " + x)
