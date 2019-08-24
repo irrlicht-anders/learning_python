dc0b27e43235c881054c3df2d412ecceb662958e
irrlicht-anders <irrlicht.anders@posteo.me>
2019-08-24 17:20:16 +0200

    added addtional line, so you can see wether your input is zero OR a negative number :)

--- number_posneg.py
+++ number_posneg.py
@@ -1,10 +1,15 @@
 # Program checks wether the number is negative
 # or not and displays an approbiate message
+------------------------------------------------
+# added additional line whicht checks wether the
+# input is zero OR negative
 
 num = input("Please enter a postive or negative number! ")
 a = int(num) #convert from string to int
 
-if a >= 0:
+if a > 0:
 	print("Postive or Zero")
+elif num == 0:
+	print("Zero")
 else:
 	print("Negative number")
